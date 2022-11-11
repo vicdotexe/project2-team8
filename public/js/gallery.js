a9ac5321f8ee431646e356e00ce7288ed4f35644
victor <vicdotexe@gmail.com>
2022-11-11 01:37:36 -0800

    major overhaul of gallery view, both styling and responsiveness, aswell as better UX regarding art data and interraction

---
+++ public/js/gallery.js
@@ -0,0 +1,50 @@
+const galleryItems = document.querySelectorAll(".galleryItem");
+const stars = document.querySelectorAll(".galleryItem");
+
+galleryItems.forEach(item=> item.addEventListener("click", (e)=>{
+
+    if (e.target.classList.contains("star")){
+        const id = e.target.getAttribute('data-id');
+        const isLiked = e.target.getAttribute('data-liked') == "true";
+        if (isLiked){
+            unlike(id);
+        }else{
+            like(id);
+        }
+    }else{
+        const id = item.getAttribute('data-id');
+        document.location.href=`/artpiece/${id}`;
+    }
+}))
+
+const unlike = (id)=>{
+    fetch('/api/likes',{
+        method:'DELETE',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ArtPieceId:id})
+    }).then(response=>{
+        if (response.ok){
+            document.location.reload();
+        }else{
+            document.location.replace('/signin');
+        }
+    })
+}
+
+const like = (id)=>{
+    fetch('/api/likes',{
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ArtPieceId:id})
+    }).then(response=>{
+        if (response.ok){
+            document.location.reload();
+        }else{
+            document.location.replace('/signin');
+        }
+    })
+}
